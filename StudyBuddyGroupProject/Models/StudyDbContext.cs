316491038d37ff51966581ff4de327f6fee0a892
belle <belle.baxley@gmail.com>
2023-03-09 10:26:40 -0500

    fixed accordion

--- StudyBuddyGroupProject/Models/StudyDbContext.cs
+++ StudyBuddyGroupProject/Models/StudyDbContext.cs
@@ -21,7 +21,15 @@ public partial class StudyDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=StudyDB;Trusted_Connection=True;Encrypt=False");
+
+    //BELLE
+    => optionsBuilder.UseSqlServer("Server=localhost,1433; Initial Catalog=StudyDB; User ID=SA; Password=EnterPasswordHere9@; TrustServerCertificate=true;");
+    //ETHAN
+    //=> optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=StudyDB;Trusted_Connection=True;Encrypt=False");
+    //CAM
+    //=> optionsBuilder.UseSqlServer("Server=localhost,1433; Initial Catalog=StudyDB; User ID=SA; Password=EnterPasswordHere1; TrustServerCertificate=true;");
+    //CARISSA
+    //=> optionsBuilder.UseSqlServer("Server=localhost,1433; Initial Catalog=StudyDB; User ID=SA; Password=GrandCircus1!; TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
